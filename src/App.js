d291196c1715c31c396a8746b5edd0cc2b2b3557
martovich <mart-1984@mail.ru>
2020-03-04 00:41:11 +0300

    first init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
+const App = () => (
+    <div className='app-wrapper'>
+        <Header/>
+        <Navbar/>
+        <Profile/>
+    </div>
+);
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => (
+    <div className='content'>
+        <div>
+            <img src='https://www.seonews.ru/upload/medialibrary/2d7/2d7a50bf0f6a956bbfcc00b13e25dab2.jpg'/>
+        </div>
+        <div>
+            avatar + discription
+        </div>
+        <div>
+            My Posts
+            <div>
+                New Post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+);
+export default Profile
